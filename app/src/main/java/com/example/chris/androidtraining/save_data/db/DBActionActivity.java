ca74d2c04c8beeade07afc26692d5ea917d258d2
Anima18 <591151451@qq.com>
2018-01-16 23:25:56 +0800

    save db data

---
+++ app/src/main/java/com/example/chris/androidtraining/save_data/db/DBActionActivity.java
@@ -0,0 +1,17 @@
+package com.example.chris.androidtraining.save_data.db;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Admin on 2018/1/16.
+ * https://zhuanlan.zhihu.com/p/29472803
+ */
+
+public class DBActionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
